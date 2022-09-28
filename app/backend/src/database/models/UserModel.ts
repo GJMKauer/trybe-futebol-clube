c5fcce31d41205f2739c660580ada14b0bab4f2b
Gabriel Kauer <gjmkauer@hotmail.com>
2022-09-28 15:50:48 -0300

    FEAT: add req1 - create UserModel and User Migration. Seed users table.

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,30 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: DataTypes.STRING,
+  role: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default User;
